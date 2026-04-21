ee0690efb097430a734a57058fe21a2c0e5012d6
Rayyan-Alam71 <alamrayyan167@gmail.com>
2026-04-21 20:50:48 +0530

    updating cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin : "*"
+}));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
